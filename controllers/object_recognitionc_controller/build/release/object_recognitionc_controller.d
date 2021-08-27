cae154a6d38ebfeab66a5b75becbd0ed279e1083
KHALED AHMMED ANIK <59437962+Khaled227@users.noreply.github.com>
2021-08-27 16:38:42 +0600

    complete

---
+++ controllers/object_recognitionc_controller/build/release/object_recognitionc_controller.d
@@ -0,0 +1,9 @@
+build/release/object_recognitionc_controller.o: \
+ object_recognitionc_controller.cpp \
+ C:\Users\user\AppData\Local\Programs\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
+ C:\Users\user\AppData\Local\Programs\Webots/include/controller/cpp/webots/Device.hpp \
+ C:/Users/user/AppData/Local/Programs/Webots/include/controller/c/webots/types.h \
+ C:\Users\user\AppData\Local\Programs\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Users\user\AppData\Local\Programs\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:\Users\user\AppData\Local\Programs\Webots/include/controller/cpp/webots/Camera.hpp \
+ C:/Users/user/AppData/Local/Programs/Webots/include/controller/c/webots/camera_recognition_object.h
